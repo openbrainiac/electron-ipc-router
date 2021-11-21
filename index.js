0db1eab85655df32978a3625c9a072df9ebf69f4
NadirTellai <tellainadir@gmail.com>
2021-11-21 10:34:25 +0100

    unique event

--- index.js
+++ index.js
@@ -19,35 +19,33 @@ function subscriber({routes,server, port}){
                 console.error(err)
                 event.reply(`${route}-${id}-error`, err)
             }
-            if(!(result instanceof Promise)) event.returnValue =  result
+            if(!(result instanceof Promise)) event.reply(`${route}-${id}-response`, result)
 
             else result
-                    .then(response => {
-                        event.reply(`${route}-${id}-response`, response);
-                    })
-                    .catch(e => {
-                        console.error(e);
-                        event.reply(`${route}-${id}-error`, e);
-                    });
+              .then(response => {
+                  event.reply(`${route}-${id}-response`, response);
+              })
+              .catch(e => {
+                  console.error(e);
+                  event.reply(`${route}-${id}-error`, e);
+              });
 
         });
     });
     if(server && !port) throw new Error("port number not found");
-    else if(server && port) server(routes, port);
+    else if(server && port) launchServer(routes, port);
 }
 
 function ipc(route, ...args) {
-    let syncResponse = ipcRenderer.sendSync(route, ...args)
-    if(syncResponse) return syncResponse
-     return new Promise(((resolve, reject) => {
-         let id = Math.random()
-         ipcRenderer.once(`${route}-${id}-response`, (event, response) => {
-             ipcRenderer.removeAllListeners(`${route}-${id}-error`);
+    return new Promise(((resolve, reject) => {
+        let id = Math.random()
+        ipcRenderer.once(`${route}-${id}-response`, (event, response) => {
+            ipcRenderer.removeAllListeners(`${route}-${id}-error`);
             resolve(response);
         });
 
-         ipcRenderer.once(`${route}-${id}-error`, (event, error) => {
-             ipcRenderer.removeAllListeners(`${route}-${id}-response`);
+        ipcRenderer.once(`${route}-${id}-error`, (event, error) => {
+            ipcRenderer.removeAllListeners(`${route}-${id}-response`);
             reject(error);
         });
 
@@ -55,7 +53,8 @@ function ipc(route, ...args) {
     }))
 }
 
-function server(routes, port){
+
+function launchServer(routes, port){
     http.createServer((request, response) => {
         const { headers, method, url } = request;
         if(method !== "POST") return sendError(response, "method must be POST");
@@ -67,37 +66,37 @@ function server(routes, port){
 
         let body = [];
         request
-            .on('error', (err) => {
-                console.error(err);
-            })
+          .on('error', (err) => {
+              console.error(err);
+          })
 
-            .on('data', (chunk) => body.push(chunk))
+          .on('data', (chunk) => body.push(chunk))
 
-            .on('end', () => {
-                if(!body.length) return sendError(response, "body is required");
-                body = Buffer.concat(body).toString();
-                let args = JSON.parse(body).args
-                if(!args) return sendError(response, "args are missing from the body");
-                if(!Array.isArray(args)) return  sendError(response, "args must be an array");
+          .on('end', () => {
+              if(!body.length) return sendError(response, "body is required");
+              body = Buffer.concat(body).toString();
+              let args = JSON.parse(body).args
+              if(!args) return sendError(response, "args are missing from the body");
+              if(!Array.isArray(args)) return  sendError(response, "args must be an array");
 
-                let result
-                try { result = cb(...args)}
-                catch (err){
-                    console.error(err)
-                    sendError(response, JSON.stringify(err, Object.getOwnPropertyNames(err)))
-                    return
-                }
+              let result
+              try { result = cb(...args)}
+              catch (err){
+                  console.error(err)
+                  sendError(response, JSON.stringify(err, Object.getOwnPropertyNames(err)))
+                  return
+              }
 
-                if(!(result instanceof Promise)) sendSuccess(response, result)
+              if(!(result instanceof Promise)) sendSuccess(response, result)
 
-                else result
-                    .then(res => {
-                        sendSuccess(response, res)
-                    })
-                    .catch(e => {
-                        sendError(response, JSON.stringify(e, Object.getOwnPropertyNames(e)))
-                    });
-            });
+              else result
+                .then(res => {
+                    sendSuccess(response, res)
+                })
+                .catch(e => {
+                    sendError(response, JSON.stringify(e, Object.getOwnPropertyNames(e)))
+                });
+          });
     })
       .listen(port,function() {
           console.log('\x1b[32m%s\x1b[0m', 'electron-ipc-router server running on port ' + port);
